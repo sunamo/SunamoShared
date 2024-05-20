0fe40ea2d9b6a2edf7be4467a2b743e370ccf631
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-20 08:08:49 +0200

    Some manually changes

--- Args/GetFilesEveryFolder.cs
+++ Args/GetFilesEveryFolder.cs
@@ -1,6 +1,10 @@
-namespace SunamoShared;
-
-
+namespace
+#if SunamoFileSystem
+SunamoFileSystem
+#else
+SunamoShared
+#endif
+;
 public class GetFilesEveryFolder : GetFilesMoreMascArgs
 {
     public bool _trimA1 = false;
