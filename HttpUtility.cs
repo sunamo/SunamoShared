23f0c653e3c20f4c74e4fc0df360f6db6959b1c1
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-20 19:00:18 +0200

    Before ending build all

--- HttpUtility.cs
+++ HttpUtility.cs
@@ -1,15 +1,14 @@
 using System.Collections.Specialized;
 using System.Net;
-
 namespace
 #if SunamoDevCode
 SunamoDevCode
+#elif SunamoConverters
+SunamoConverters
 #else
 SunamoShared
 #endif
 ;
-
-
 /// <summary>
 ///     Není ve sunamo, tím pádem nebudu dávat do NS
 ///     Třída byla vytvořena abych nemusel importovat System.Web pro metody jež nejsou v WebUtility
@@ -19,19 +18,15 @@ public class HttpUtility : HttpUtilitySE
     public static NameValueCollection ParseQueryString(string responseContent)
     {
         return HttpUtilitySE.ParseQueryString(responseContent);
-
     }
-
     public static string HtmlDecode(string v)
     {
         return WebUtility.HtmlDecode(v);
     }
-
     public static string HtmlEncode(string html)
     {
         return HtmlEncodeWithCompatibility(html);
     }
-
     public static string HtmlEncodeWithCompatibility(string html, bool backwardCompatibility = true)
     {
         if (html == null)
@@ -44,12 +39,10 @@ public class HttpUtility : HttpUtilitySE
             : new Regex("&(?!(amp;)|(lt;)|(gt;)|(quot;)|(nbsp;)|(reg;))", RegexOptions.IgnoreCase);
         return rx.Replace(html, "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("\"", "&quot;");
     }
-
     public static string UrlEncode(string slnName)
     {
         return WebUtility.UrlEncode(slnName);
     }
-
     public static string UrlDecode(string v)
     {
         return WebUtility.UrlDecode(v);
