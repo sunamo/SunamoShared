f17d9802bf3d518c0ecc96b1d9c2bdd110a70526
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:50 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoEnums/Enums/AppFoldersShared.cs
@@ -0,0 +1,24 @@
+namespace SunamoShared;
+
+
+internal enum AppFoldersShared
+{
+    #region Not backuped
+    Logs,
+    Output,
+    Cache,
+    Temp,
+    #endregion
+    #region Backuped
+    Input,
+    Settings,
+    Data,
+    Other,
+    Controls,
+    Local,
+    Roaming,
+    Crypted,
+    Reports,
+    Backup
+    #endregion
+}
