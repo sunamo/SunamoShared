9a06c89b7370e2aca39e6595f1d0829ee534cfeb
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-16 12:28:31 +0200

    Removed unused file

--- _SunamoExceptions/ai/AIAssembly.cs
+++ _SunamoExceptions/ai/AIAssembly.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SunamoShared._SunamoExceptions.ai;
+﻿namespace SunamoShared._SunamoExceptions.ai;
 public class AIAssembly<T>
 {
     public object t { get; set; }
