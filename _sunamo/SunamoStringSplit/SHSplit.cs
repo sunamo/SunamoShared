7062b460e2ae4d578658793bae0ce25598f8fa77
sunamo.cz <radek.jancik@sunamo.cz>
2025-05-10 09:30:24 +0200

    Simplify SHSplit API

--- _sunamo/SunamoStringSplit/SHSplit.cs
+++ _sunamo/SunamoStringSplit/SHSplit.cs
@@ -16,7 +16,7 @@ internal class SHSplit
         return v1.Split(v2).ToList();
     }
 
-    internal static List<string> SplitMore(string p, params string[] newLine)
+    internal static List<string> Split(string p, params string[] newLine)
     {
         return p.Split(newLine, StringSplitOptions.RemoveEmptyEntries).ToList();
     }
